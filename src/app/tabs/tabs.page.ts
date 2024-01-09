6d4be24f014f63659267f39c0a53498fa17f0e12
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-09 07:52:17 +0100

    icon added

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {
+  IonIcon,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  chatboxEllipsesOutline,
+  heartOutline,
+  homeOutline,
+  personOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon],
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      chatboxEllipsesOutline,
+      heartOutline,
+      homeOutline,
+      personOutline,
+    });
   }
 
+  ngOnInit() {}
 }
