ee240d3b6be865d62dab41c3b438594089d7270e
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-09 08:30:52 +0100

    Add menu buttons to home-page and give it ✨STYLE✨

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonThumbnail,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { appsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonContent, IonTitle],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonContent,
+    IonTitle,
+    IonButtons,
+    IonThumbnail,
+    IonIcon,
+  ],
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  constructor() {
+    addIcons({ appsOutline });
+  }
 
   ngOnInit() {}
 }
