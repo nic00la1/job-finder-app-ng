9b1eee441911263b07c71c0bde7084adbe853baf
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-09 10:19:04 +0100

    Update job descriptions in JobDetailPage

--- src/app/tabs/job-detail/job-detail.page.ts
+++ src/app/tabs/job-detail/job-detail.page.ts
@@ -73,7 +73,7 @@ export class JobDetailPage implements OnInit {
         logo_light: 'tiktok_light.png',
         image: 'ux.jpg',
         description: [
-          '3 or more years of UX design experience. Preference will be given to candidates who have experience designing complex solutions.',
+          '3 or more years of UX design experience. Preference will be given to candidates who have experience designing engaging and interactive solutions.',
         ],
       },
       {
@@ -88,7 +88,7 @@ export class JobDetailPage implements OnInit {
         logo_light: 'uber_light.png',
         image: 'developer.jpg',
         description: [
-          '5 or more years of experience needed as a Senior developer in Full Stack. Preference will be given to candidates who have experience designing complex solutions.',
+          '5 or more years of experience needed as a Senior developer in Full Stack. Preference will be given to candidates who have experience designing complex solutions and working in a fast-paced environment.',
         ],
       },
       {
@@ -103,22 +103,7 @@ export class JobDetailPage implements OnInit {
         logo_light: 'linkedin_light.png',
         image: 'ux2.jpg',
         description: [
-          '4 or more years of UX design experience. Preference will be given to candidates who have experience designing complex solutions.',
-        ],
-      },
-      {
-        id: 4,
-        company: 'TikTok',
-        location: 'New Delhi',
-        expires_on: '30/11/23',
-        post: 'Senior UX Designer',
-        type: 'Full Time',
-        salary: '$40-90k/year',
-        logo_dark: 'tiktok_dark.png',
-        logo_light: 'tiktok_light.png',
-        image: 'ux1.jpg',
-        description: [
-          '3 or more years of UX design experience. Preference will be given to candidates who have experience designing complex solutions.',
+          '4 or more years of UX design experience. Preference will be given to candidates who have experience designing complex solutions for large-scale companies.',
         ],
       },
     ];
